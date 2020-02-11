1bdbd65d0d7b769cac7d7b952001b3dc6afd29a1
Mark Albiston <mark.albiston@gmail.com>
2020-02-10 21:50:30 -0700

    initial commit

---
+++ MyName.java
@@ -0,0 +1,7 @@
+public class MyName {
+    public static void main(String[] args){
+        System.out.println("My Name is Mark");
+        System.out.println("I am 34 years old");
+        System.out.println("My hometown is Boise, ID");
+    }
+}
